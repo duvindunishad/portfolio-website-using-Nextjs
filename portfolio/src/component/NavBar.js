1b2130ff98cbee7721a8358904f789604e038a26
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-07-11 14:08:51 +0530

    update nav links

---
+++ portfolio/src/component/NavBar.js
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import React from 'react'
+import Logo from './Logo'
+
+const CustomLink = ({href,title,className=""})=>{
+    return(
+        <Link href ={href}>
+            {title}
+        </Link>
+    )
+}
+const NavBar = () => {
+  return (
+    <header
+    className='w-full px-32 py-8 font-medium flex items-center justify-center'>
+      <nav>
+        <CustomLink href = "/" title="Home"/>
+        <CustomLink href = "/about" title="About"/>
+        <CustomLink href = "/projects" title="Projects"/>
+        <CustomLink href = "/articles" title="Articles"/>
+      </nav>
+      <nav>
+        <Link href="/" target={"_blank"}>Twitter</Link>
+        <Link href="/" target={"_blank"}>Twitter</Link>
+        <Link href="/" target={"_blank"}>Twitter</Link>
+        <Link href="/" target={"_blank"}>Twitter</Link>
+        <Link href="/" target={"_blank"}>Twitter</Link>
+        <Link href="/" target={"_blank"}>Twitter</Link>
+      </nav>
+      <div className='absolute left-[50%] top-2 translate-x-[-50%] ' >
+      <Logo/>
+      </div>
+    </header>
+  )
+}
+
+export default NavBar
--- portfolio/src/pages/_app.js
+++ portfolio/src/pages/_app.js
@@ -1,3 +1,4 @@
+import NavBar from "@/component/NavBar";
 import "@/styles/globals.css";
 import {Montserrat} from "next/font/google";
 
@@ -9,7 +10,8 @@ const montserrat = Montserrat({
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${montserrat.variable} font-mont  bg-light w-full min-h-screen`}>
-<Component {...pageProps} />
+  <NavBar/>
+  <Component {...pageProps} />
   </main>
   )
 }
