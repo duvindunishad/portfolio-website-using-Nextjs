5354e0d70a5e54e94bd2058e4bea2464e8821121
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-07-11 14:25:55 +0530

    update nav bar with adding underline animation

--- portfolio/src/component/NavBar.js
+++ portfolio/src/component/NavBar.js
@@ -1,23 +1,28 @@
 import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
+import { useRouter } from 'next/router'
 
 const CustomLink = ({href,title,className=""})=>{
+    const router = useRouter();
     return(
-        <Link href ={href}>
+        <Link href ={href} className={`${className} relative group`}>
             {title}
+            <span className={`h-[1px] inline-block  bg-dark
+            absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ese duration-300
+            ${router.asPath === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
         </Link>
     )
 }
 const NavBar = () => {
   return (
     <header
-    className='w-full px-32 py-8 font-medium flex items-center justify-center'>
+    className='w-full px-32 py-8 font-medium flex items-center justify-between'>
       <nav>
-        <CustomLink href = "/" title="Home"/>
-        <CustomLink href = "/about" title="About"/>
-        <CustomLink href = "/projects" title="Projects"/>
-        <CustomLink href = "/articles" title="Articles"/>
+        <CustomLink href = "/" title="Home" className='mr-4'/>
+        <CustomLink href = "/about" title="About" className='mr-4'/>
+        <CustomLink href = "/projects" title="Projects" className='mr-4'/>
+        <CustomLink href = "/articles" title="Articles" className='ml-4'/>
       </nav>
       <nav>
         <Link href="/" target={"_blank"}>Twitter</Link>
